fe3f627b43d126fc2be81483542223f22c216bd8
basic <pozzzitivu@mail.ru>
2022-03-06 12:26:36 +0400

    added 147

--- chapter9Arrays/forEach/script.js
+++ chapter9Arrays/forEach/script.js
@@ -1,27 +1,40 @@
 'use strict';
 
-// Immidiately Invoked Functions Expression (IIFE) немедленно вызываемые функции экпрешн//////////////////////////////
-const runOneTime = function () {
-  console.log(`You will never see this function call again.`);
-};
-// runOneTime();
-// runOneTime();
+// Итерация с помощью forEach /////////////////
 
-// инкапсуляция, данные во внутреннем скоупе - приватные
-(function () {
-  console.log(`You will never see this function call again.`);
-  const xxx = 1; // переменная ххх инкапсулирована в скоупе, эта переменная таким образом защищена от изменения из внешних скоупов
-  var z = 3; // iife были придуманы чтобы не дать доступ к переменной вар из внешнего скоупа, это было когда еще не было конст и лет
-})(); // для вызова надо указать пару скобок в конце как у функции
+const currencies = new Map([
+  ['USD', 'Inited States dollar'],
+  ['EUR', 'Euro'],
+  ['CNY', 'Chinese yuan'],
+]);
 
-(() => console.log(`You will never see this ARROW function call again.`))();
+const transactions = [300, 250, -500, 5000, -750, -180, 50, 1400, -350, -120];
 
-// console.log(xxx); // будет ошибка, т.к. ххх нет в области видимости глобальном скоупе, глобальный скоуп не имеет доступа к внутреннему скопу функции, она только внутри функции
+// for (const transaction of transactions) {
+//   if (transaction > 0) {
+//     console.log(`${transaction} was deposited`);
+//   } else {
+//     console.log(`${Math.abs(transaction)} was withdrew`);
+//   }
+// }
 
-{
-  const yyy = 1; // к этой нет доступа из внешнего скоупа
-  var y = 2; // к этой есть доступ, можно вывести например в консоль
-}
+// получение доступа к индексу элемента в массиве
+// for (const [i, transaction] of transactions.entries()) {
+//   if (transaction > 0) {
+//     console.log(`${transaction} was deposited`);
+//     console.log(i);
+//   } else {
+//     console.log(`${Math.abs(transaction)} was withdrew`);
+//     console.log(i);
+//   }
+// }
 
-console.log(y);
+transactions.forEach(function (transaction, index, array) {
+  if (transaction > 0) {
+    console.log(`Tansaction № ${index + 1}: ${transaction} was deposited`);
+  } else {
+    console.log(`Tansaction № ${index + 1}: ${Math.abs(transaction)} was withdrew`);
+  }
+});
+// ключевое отличие, что для фор ич нельзя использовать прерывания, брэйк
 
