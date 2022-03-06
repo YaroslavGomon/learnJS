d6b119da3ac470d0c0eaa70594d15a56955cc22f
basic <pozzzitivu@mail.ru>
2022-03-06 12:40:20 +0400

    added 148

--- chapter9Arrays/forEach/script.js
+++ chapter9Arrays/forEach/script.js
@@ -2,12 +2,6 @@
 
 // Итерация с помощью forEach /////////////////
 
-const currencies = new Map([
-  ['USD', 'Inited States dollar'],
-  ['EUR', 'Euro'],
-  ['CNY', 'Chinese yuan'],
-]);
-
 const transactions = [300, 250, -500, 5000, -750, -180, 50, 1400, -350, -120];
 
 // for (const transaction of transactions) {
@@ -29,12 +23,30 @@ const transactions = [300, 250, -500, 5000, -750, -180, 50, 1400, -350, -120];
 //   }
 // }
 
-transactions.forEach(function (transaction, index, array) {
-  if (transaction > 0) {
-    console.log(`Tansaction № ${index + 1}: ${transaction} was deposited`);
-  } else {
-    console.log(`Tansaction № ${index + 1}: ${Math.abs(transaction)} was withdrew`);
-  }
-});
+// transactions.forEach(function (transaction, index, array) {
+//   if (transaction > 0) {
+//     console.log(`Tansaction № ${index + 1}: ${transaction} was deposited`);
+//   } else {
+//     console.log(`Tansaction № ${index + 1}: ${Math.abs(transaction)} was withdrew`);
+//   }
+// });
 // ключевое отличие, что для фор ич нельзя использовать прерывания, брэйк
 
+// forEach with Map and Set ///////////////////
+
+const currencies = new Map([
+  ['USD', 'Inited States dollar'],
+  ['EUR', 'Euro'],
+  ['CNY', 'Chinese yuan'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+const uniqueCurrencies = new Set(['USD', 'EUR', 'USD', 'CNY', 'CNY']);
+console.log(uniqueCurrencies);
+
+uniqueCurrencies.forEach(function(value, _, set) {
+  console.log(`${value}: ${value}`);
+})
