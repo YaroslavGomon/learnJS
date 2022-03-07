4124414ce49d8e3c057def6bdcbb8f1d909cefd3
basic <pozzzitivu@mail.ru>
2022-03-07 23:51:25 +0400

    added 156

--- chapter9Arrays/filterMethod/script.js
+++ chapter9Arrays/filterMethod/script.js
@@ -1,2 +1,20 @@
 'use strict';
 
+const transactions = [300, 250, -500, 5000, -750, -180, 50, 1400, -350, -120];
+
+const withdrawals = transactions.filter(value => value < 0);
+
+console.log(withdrawals);
+
+const withdrawals1 = [];
+for (const trans of transactions) {
+  if (trans < 0) {
+    withdrawals1.push(trans);
+  }
+}
+
+console.log(withdrawals1);
+
+const deposites = transactions.filter(value => value > 0);
+
+console.log(deposites);
