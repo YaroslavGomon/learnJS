3a6bc92014f3503a895be30234e42af356fe137b
basic <pozzzitivu@mail.ru>
2022-03-11 16:58:01 +0400

    added 173

--- chapter9Arrays/simpleBankApp/script.js
+++ chapter9Arrays/simpleBankApp/script.js
@@ -65,10 +65,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseNickname = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayTransactions = function (transactions) {
+const displayTransactions = function (transactions, sort = false) {
   containerTransactions.innerHTML = '';
 
-  transactions.forEach(function (trans, index) {
+  const transacs = sort ? transactions.slice().sort((x, y) => x - y) : transactions;
+
+  transacs.forEach(function (trans, index) {
     const transType = trans > 0 ? 'deposit' : 'withdrawal';
 
     const transactionRow = `
@@ -224,3 +226,13 @@ btnLoan.addEventListener('click', function (e) {
   }
   inputLoanAmount.value = '';
 });
+
+let transactionSorted = false;
+
+btnSort.addEventListener('click', function(e) {
+  e.preventDefault();
+  displayTransactions(currentAccount.transactions, !transactionSorted);
+  transactionSorted = !transactionSorted;
+});
+
+
