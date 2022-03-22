fff2c67b02789948247ebec37befc6018341a16d
basic <pozzzitivu@mail.ru>
2022-03-22 10:01:32 +0400

    add 185

---
+++ chapter10DateTimers/p185Remainder/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+console.log(7 % 3); // 7 = 3 * 2 + 1
+console.log(7 / 3);
+console.log(7 % 4);
+
+console.log(8 % 2);
+console.log(8764 % 2);
+
+const isNumberOdd = x => x % 2 !== 0;
+console.log(isNumberOdd(5));
+console.log(isNumberOdd(7));
+console.log(isNumberOdd(6));
+console.log(isNumberOdd(50));
