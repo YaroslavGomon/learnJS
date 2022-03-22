361247c688fe02f58c79680d551a323554fd206c
basic <pozzzitivu@mail.ru>
2022-03-22 09:31:28 +0400

    add 183

---
+++ chapter10DateTimers/p183Math/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+console.log(Math.sqrt(9));
+console.log(9 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(3, 55, 77));
+console.log(Math.max(3, '55', '77', 44, 22));
+console.log(Math.max(3, '55%', '77%', 44, 22));
+console.log(Math.min(3, 55, 77));
+
+console.log(Math.PI);
+console.log(Math.PI * Number.parseInt('20px') ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+console.log(Math.trunc(1.99));
+
+console.log(Math.round(1.99));
+console.log(Math.round(1.34));
+
+console.log(Math.ceil(1.99));
+console.log(Math.ceil(1.34));
+
+console.log(Math.floor('1.99'));
+console.log(Math.floor(1.34));
+
+console.log(Math.trunc(-1.99));
+console.log(Math.floor(-1.99));
+
+console.log(Math.PI);
+console.log(+Math.PI.toFixed(2));
