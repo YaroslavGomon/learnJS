fa5ba518fb1b848ef46abd3b432fc5b1adc292a8
YaroslavGomon <pozzzitivu@mail.ru>
2022-05-27 22:27:24 +0400

    add 200

--- chapter11AdvancedDOM/p197BankSimple/script.js
+++ chapter11AdvancedDOM/p197BankSimple/script.js
@@ -36,3 +36,41 @@ document.addEventListener('keydown', function (e) {
     closeModalWindow();
   }
 });
+
+//////////////////////////////////////////////////
+// Выбор элементов
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+console.log(document.querySelector('.header'));
+const sections = document.querySelectorAll('.section');
+console.log(sections);
+
+console.log(document.getElementById('section--1'));
+const buttons = document.getElementsByTagName('button');
+console.log(buttons);
+
+console.log(document.getElementsByClassName('btn'));
+
+//////////////////////////////////////////////////
+// Создание и вставка элементов
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'Мы используем на этом сайте куки для улучшения фукциональности';
+
+message.innerHTML =
+  'Мы используем на этом сайте cookie для улучшения фукциональности. <button class = "btn btn--close-cookie">Ok!</button>';
+
+const header = document.querySelector('.header');
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+// header.before(message);
+// header.after(message);
+
+// Удаление элементов
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+  message.remove();
+});
