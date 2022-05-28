a2a8bec17f763d2ad49411799a214d6f2ba9cb54
YaroslavGomon <pozzzitivu@mail.ru>
2022-05-28 21:21:55 +0400

    add 202

--- chapter11AdvancedDOM/p197BankSimple/script.js
+++ chapter11AdvancedDOM/p197BankSimple/script.js
@@ -37,81 +37,110 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+// Плавная прокрутка
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', e => {
+  const section1Coords = section1.getBoundingClientRect();
+  console.log(section1Coords);
+  console.log(e.target.getBoundingClientRect());
+  console.log(
+    'Текущее прокручивание: x, y',
+    window.pageXOffset,
+    window.pageYOffset
+  );
+  console.log(
+    'Ширина и высота вьюпорта: ',
+    document.documentElement.clientWidth,
+    document.documentElement.clientHeight
+  );
+
+  // window.scrollTo(section1Coords.left, section1Coords.top + window.pageYOffset);
+  // window.scrollTo({
+  //   left: section1Coords.left,
+  //   top: section1Coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({behavior: 'smooth'})
+});
+
 //////////////////////////////////////////////////
 // Выбор элементов
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
 
-console.log(document.querySelector('.header'));
-const sections = document.querySelectorAll('.section');
-console.log(sections);
+// console.log(document.querySelector('.header'));
+// const sections = document.querySelectorAll('.section');
+// console.log(sections);
 
-console.log(document.getElementById('section--1'));
-const buttons = document.getElementsByTagName('button');
-console.log(buttons);
+// console.log(document.getElementById('section--1'));
+// const buttons = document.getElementsByTagName('button');
+// console.log(buttons);
 
-console.log(document.getElementsByClassName('btn'));
+// console.log(document.getElementsByClassName('btn'));
 
 //////////////////////////////////////////////////
 // Создание и вставка элементов
 
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-// message.textContent = 'Мы используем на этом сайте куки для улучшения фукциональности';
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// // message.textContent = 'Мы используем на этом сайте куки для улучшения фукциональности';
 
-message.innerHTML =
-  'Мы используем на этом сайте cookie для улучшения фукциональности. <button class = "btn btn--close-cookie">Ok!</button>';
+// message.innerHTML =
+//   'Мы используем на этом сайте cookie для улучшения фукциональности. <button class = "btn btn--close-cookie">Ok!</button>';
 
-const header = document.querySelector('.header');
-// header.prepend(message);
-header.append(message);
-// header.append(message.cloneNode(true));
-// header.before(message);
-// header.after(message);
+// const header = document.querySelector('.header');
+// // header.prepend(message);
+// header.append(message);
+// // header.append(message.cloneNode(true));
+// // header.before(message);
+// // header.after(message);
 
-// Удаление элементов
-document.querySelector('.btn--close-cookie').addEventListener('click', () => {
-  message.remove();
-});
+// // Удаление элементов
+// document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+//   message.remove();
+// });
 
-// Стили
-message.style.backgroundColor = '#076785';
-message.style.width = '120%';
-console.log(message.style.width);
-console.log(message.style.color);
-console.log(getComputedStyle(message).color); // так можно вытащить стиль из цсс
-console.log(getComputedStyle(message).height);
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height) + 50 + 'px';
+// // Стили
+// message.style.backgroundColor = '#076785';
+// message.style.width = '120%';
+// console.log(message.style.width);
+// console.log(message.style.color);
+// console.log(getComputedStyle(message).color); // так можно вытащить стиль из цсс
+// console.log(getComputedStyle(message).height);
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height) + 50 + 'px';
 
-document.documentElement.style.setProperty('--color-first', 'yellow');
+// document.documentElement.style.setProperty('--color-first', 'yellow');
 
-// Атрибуты
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.getAttribute('src'));
-console.log(logo.className);
+// // Атрибуты
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.getAttribute('src'));
+// console.log(logo.className);
 
-logo.alt = 'Лого Банка';
+// logo.alt = 'Лого Банка';
 
-// Нестандартный атрибут
-console.log(logo.getAttribute('developer'));
-logo.setAttribute('copyright', 'Masters Of Code')
+// // Нестандартный атрибут
+// console.log(logo.getAttribute('developer'));
+// logo.setAttribute('copyright', 'Masters Of Code')
 
-const link = document.querySelector('.nav__link--btn');
-console.log(link.href);
-console.log(link.getAttribute('href'));
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
 
-// Data attributes
-console.log(logo.dataset.versionNumber);
+// // Data attributes
+// console.log(logo.dataset.versionNumber);
 
-// Classes
-logo.classList.add('a', 'b');
-logo.classList.remove('a', 'b');
-logo.classList.toggle('a');
-logo.classList.contains('c');
+// // Classes
+// logo.classList.add('a', 'b');
+// logo.classList.remove('a', 'b');
+// logo.classList.toggle('a');
+// logo.classList.contains('c');
 
-// Не использовать
-logo.className = 'a';
+// // Не использовать
+// logo.className = 'a';
