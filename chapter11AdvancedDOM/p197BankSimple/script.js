a8eb41017c0cd42c76c2217362d348e471305c8f
YaroslavGomon <pozzzitivu@mail.ru>
2022-05-28 20:45:19 +0400

    add 201

--- chapter11AdvancedDOM/p197BankSimple/script.js
+++ chapter11AdvancedDOM/p197BankSimple/script.js
@@ -74,3 +74,44 @@ header.append(message);
 document.querySelector('.btn--close-cookie').addEventListener('click', () => {
   message.remove();
 });
+
+// Стили
+message.style.backgroundColor = '#076785';
+message.style.width = '120%';
+console.log(message.style.width);
+console.log(message.style.color);
+console.log(getComputedStyle(message).color); // так можно вытащить стиль из цсс
+console.log(getComputedStyle(message).height);
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 50 + 'px';
+
+document.documentElement.style.setProperty('--color-first', 'yellow');
+
+// Атрибуты
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+console.log(logo.className);
+
+logo.alt = 'Лого Банка';
+
+// Нестандартный атрибут
+console.log(logo.getAttribute('developer'));
+logo.setAttribute('copyright', 'Masters Of Code')
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('a', 'b');
+logo.classList.remove('a', 'b');
+logo.classList.toggle('a');
+logo.classList.contains('c');
+
+// Не использовать
+logo.className = 'a';
