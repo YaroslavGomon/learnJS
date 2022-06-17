fe185aa7f9556141f459e953dee7704e51bc13c2
YaroslavGomon <pozzzitivu@mail.ru>
2022-06-17 22:40:11 +0400

    add 240

---
+++ chapter12OOP/240Incapsulation2/script.js
@@ -0,0 +1,77 @@
+'use strict';
+
+// Public fields
+// Private fields
+// Public methods
+// Private methods
+
+class Account {
+  // Public fields
+  local = navigator.language;
+
+  // Private fields
+  #transactions = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    // Protected property, with "_"
+    this.#pin = pin;
+    // this._transactions = [];
+    // this.local = navigator.language;
+
+    console.log(`ЙОу!!! ${owner}`);
+  }
+
+  // Public interface - API
+  deposit(value) {
+    this.#transactions.push(value);
+  }
+
+  withdraw(value) {
+    // this.transactions.push(-value);
+    this.deposit(-value);
+  }
+
+  setDefaultPin() {
+    this.#pin = '0000';
+  }
+
+  getTransactions() {
+    return this.#transactions;
+  }
+
+  requestLoan(value) {
+    if (this.#approveLoan(value)) {
+      this.deposit(value);
+      console.log(`Займ утвержден!`);
+    }
+  }
+
+  static greet() {
+    console.log(`Добро пожаловать в "Просто Банк"!`);
+  }
+
+  // Private methods
+  // Protected Methods
+  #approveLoan(value) {
+    return true;
+  }
+}
+
+const account1 = new Account('Jack', 'USD', 1111);
+
+// account1.#transactions.push(500);
+// account1.#transactions.push(-100);
+
+account1.deposit(500);
+account1.withdraw(100);
+account1.requestLoan(10000);
+// account1.#approveLoan(10000); // не может быть использован снаружи класса
+account1.setDefaultPin();
+
+console.log(account1);
+// console.log(account1.#pin);
+console.log(account1.getTransactions());
+Account.greet();
